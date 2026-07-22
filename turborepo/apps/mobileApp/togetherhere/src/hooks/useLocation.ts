3efc1cad56fbf81be620104fc3009249ba1b4f0c
Callum Acourt <callumacourtt@gmail.com>
2026-07-22 19:19:01 +0100

    feat: useLocation hook for voice note logic later

---
+++ turborepo/apps/mobileApp/togetherhere/src/hooks/useLocation.ts
@@ -0,0 +1,87 @@
+import { useState, useEffect, useRef } from "react";
+import * as location from "expo-location";
+import havesrine from "haversine-distance";
+
+type Coordinate = {
+    lat: number,
+    lng: number
+}
+
+export default function useLocation () {
+    
+    const [ userLocation, setUserLocation ] = useState<location.LocationObject | null>(null);
+    const locationSubscription = useRef<location.LocationSubscription | null>(null);
+    const [ error, setError ] = useState<string | null>(null);
+    const watching = useRef<boolean>(false);
+
+    function locationCallback (location: location.LocationObject) {
+        setUserLocation(location);
+    };
+
+    async function startWatching () {
+        if (watching.current === true) return;
+        const { status } = await location.requestForegroundPermissionsAsync();
+
+        if (status !== 'granted') {
+            setError("Location access denied");
+        }
+        
+        locationSubscription.current = await location.watchPositionAsync(
+            {accuracy: location.Accuracy.BestForNavigation},
+            locationCallback
+        );
+
+        watching.current = true;
+    }
+
+    function stopWatching () {
+        locationSubscription.current?.remove();
+        watching.current = false;
+    }
+    
+    async function getLocationOnce () {
+        const { status } = await location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            setError('Location access denied');
+            return null;
+        };
+
+        try {
+            const current = await location.getCurrentPositionAsync({
+                accuracy: location.Accuracy.BestForNavigation
+            });
+            setUserLocation(current);
+            return current;
+        } catch (e) {
+            setError("Could not determine location");
+            return null
+        };
+    };
+
+    function withinDistance (target : Coordinate, radiusMeters = 15) {
+        if (!userLocation || !userLocation.coords.accuracy) return;
+        return havesrine(target, userLocation.coords) <= radiusMeters + userLocation.coords.accuracy;
+    }
+
+
+    useEffect(() => {
+    (async () => {
+        startWatching();
+
+    })();
+        
+    return () => {
+        stopWatching();
+    }
+    }, []);
+    
+    return ({
+        userLocation,
+        error,
+        startWatching,
+        stopWatching,
+        getLocationOnce,
+        withinDistance
+        }
+    )
+}
